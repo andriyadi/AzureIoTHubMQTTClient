3926e49d63512f6b24023acd565044cefea499b2
erikoleary <erik.m.oleary@gmail.com>
2018-11-18 11:58:19 -0600

    Code cleanup

--- src/AzureIoTHubMQTTClient.cpp
+++ src/AzureIoTHubMQTTClient.cpp
@@ -4,34 +4,15 @@
 
 #include "AzureIoTHubMQTTClient.h"
 
-#include "sha256.h"
-#include "Base64.h"
-#include "Utils.h"
 #include <ESP8266WiFi.h>
 
 AzureIoTHubMQTTClient::AzureIoTHubMQTTClient(Client& c, String iotHubHostName, String deviceId, String deviceKey):
         iotHubHostName_(iotHubHostName), deviceId_(deviceId), deviceKey_(deviceKey), PubSubClient(c, iotHubHostName, AZURE_IOTHUB_MQTT_PORT) {
 
-    mqttCommandSubscribeTopic_ = "devices/" + deviceId + "/messages/devicebound/#";
     mqttCommandPublishTopic_ = "devices/" + deviceId + "/messages/events/";
 }
 
-AzureIoTHubMQTTClient::~AzureIoTHubMQTTClient() {
-
-}
-
-bool AzureIoTHubMQTTClient::begin() {
-
-    if (!WiFi.isConnected())	{
-        DEBUGLOG("NOT connected to internet!\n");
-        return false;
-    }
-
-    return true;
-}
-
 void AzureIoTHubMQTTClient::run() {
-
     if (!connected()) {
         doConnect();
     }
@@ -43,81 +24,37 @@ void AzureIoTHubMQTTClient::end() {
     disconnect();
 }
 
-String AzureIoTHubMQTTClient::createIotHubSASToken(char *key, String url, long expire){
-
-    url.toLowerCase();
-    if (expire == 0) {
-        expire = 2147483647; // hardcode expire to MAX unix timestamp
-    }
-
-    String stringToSign = url + "\n" + String(expire);
-
-    // START: Create signature
-    // https://raw.githubusercontent.com/adamvr/arduino-base64/master/examples/base64/base64.ino
-
-    int keyLength = strlen(key);
-
-    int decodedKeyLength = base64_dec_len(key, keyLength);
-    char decodedKey[decodedKeyLength];  //allocate char array big enough for the base64 decoded key
-
-    base64_decode(decodedKey, key, keyLength);  //decode key
-
-    Sha256.initHmac((const uint8_t*)decodedKey, decodedKeyLength);
-    Sha256.print(stringToSign);
-    char* sign = (char*) Sha256.resultHmac();
-    // END: Create signature
-
-    // START: Get base64 of signature
-    int encodedSignLen = base64_enc_len(HASH_LENGTH);
-    char encodedSign[encodedSignLen];
-    base64_encode(encodedSign, sign, HASH_LENGTH);
-
-    // SharedAccessSignature
-    return "sr=" + url + "&sig="+ urlEncode(encodedSign) + "&se=" + String(expire);
-    // END: create SAS
-}
-
 bool AzureIoTHubMQTTClient::doConnect() {
     changeEventTo(AzureIoTHubMQTTClientEventConnecting);
 
-    String mqttUname = iotHubHostName_ + "/" + deviceId_ + "/api-version=2016-11-14";
-    //DEBUGLOG(mqttPassword);
+    auto mqttUname = iotHubHostName_ + "/" + deviceId_ + "/api-version=2016-11-14";
 
-    MQTT::Connect conn = MQTT::Connect(deviceId_).set_auth(mqttUname, deviceKey_);//.set_clean_session();
-    conn.set_keepalive(10);
-    bool ret = PubSubClient::connect(conn);
+    auto conn = MQTT::Connect(deviceId_)
+        .set_auth(mqttUname, deviceKey_)
+        .set_keepalive(10);
 
-    if (ret) {
-
-        //DEBUGLOG("Connected to Azure IoT Hub\n");
+    if (PubSubClient::connect(conn)) {
         changeEventTo(AzureIoTHubMQTTClientEventConnected);
 
-        PubSubClient::callback_t cb = [=](const MQTT::Publish& p){
+        auto cb = [=](const MQTT::Publish& p){
             _onActualMqttMessageCallback(p);
         };
 
         set_callback(cb);
 
         //Directly subscribe
-        PubSubClient::subscribe(mqttCommandSubscribeTopic_);
-        //MQTT::Subscribe sub = MQTT::Subscribe(mqttCommandSubscribeTopic_, 1);
-        //PubSubClient::subscribe(sub);
-
+        PubSubClient::subscribe("devices/" + deviceId_ + "/messages/devicebound/#");
         return true;
-    } else {
 
+    }
+    else {
         DEBUGLOG("Failed to connect to Azure IoT Hub\n");
         return false;
     }
 }
 
 bool AzureIoTHubMQTTClient::sendEvent(String payload) {
-    //return PubSubClient::publish(mqttCommandPublishTopic_, payload);
     return PubSubClient::publish(MQTT::Publish(mqttCommandPublishTopic_, payload).set_qos(1).set_retain(false));
-}
-
-bool AzureIoTHubMQTTClient::sendEvent(const uint8_t *payload, uint32_t plength, bool retained) {
-    return PubSubClient::publish(mqttCommandPublishTopic_, payload, plength, retained);
 }
 
 void AzureIoTHubMQTTClient::sendEventWithKeyVal(KeyValueMap keyValMap) {
@@ -127,7 +64,7 @@ void AzureIoTHubMQTTClient::sendEventWithKeyVal(KeyValueMap keyValMap) {
 
     const int BUFFER_SIZE = JSON_OBJECT_SIZE(MAX_JSON_OBJECT_SIZE);
     StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
-    JsonObject &root = jsonBuffer.createObject();
+    auto& root = jsonBuffer.createObject();
 
     for (const auto &keyVal: keyValMap) {
         root[keyVal.first] = keyVal.second;
@@ -141,57 +78,46 @@ void AzureIoTHubMQTTClient::sendEventWithKeyVal(KeyValueMap keyValMap) {
 }
 
 void AzureIoTHubMQTTClient::_onActualMqttMessageCallback(const MQTT::Publish &msg) {
-
     //Process message
     if (msg.payload_len() > 0 && parseMessageAsJson_ && commandsHandlerMap_.size() > 0) {
-
         StaticJsonBuffer<200> jsonBuffer;
         JsonObject& json = jsonBuffer.parseObject((char*)msg.payload(), 3);
+
         if (json.success()) {
             String key = "";
-            if (json.containsKey("Name")) {
+
+            if (json.containsKey("Name"))
                 key = "Name";
-            }
-            else if (json.containsKey("name")) {
+            else if (json.containsKey("name"))
                 key = "name";
-            }
 
             if (!key.equals("")) {
                 String cmdName = String(json[key].as<char*>());
 
                 for (const auto &myPair : commandsHandlerMap_) {
-                    if (!cmdName.equals(myPair.first)) {
+                    if (!cmdName.equals(myPair.first))
                         continue;
-                    }
 
                     DEBUGLOG("Found %s command\n", cmdName.c_str());
                     AzureIoTHubMQTTClientCommandCallback cb = myPair.second;
-//                    auto val = json[myPair.first];
-//                    cb(myPair.first, val);
                     cb(myPair.first, json);
                 }
             }
         }
     }
 
-    //Last resort
-    if (onSubscribeCallback_) {
+    // Last resort
+    if (onSubscribeCallback_)
         onSubscribeCallback_(msg);
-    }
 }
 
 void AzureIoTHubMQTTClient::onCloudCommand(String command,
                                            AzureIoTHubMQTTClient::AzureIoTHubMQTTClientCommandCallback callback) {
-
     parseMessageAsJson_ = true;
-    if (commandsHandlerMap_.size() == 0) {
-    }
-
     commandsHandlerMap_[command] = callback;
 }
 
 void AzureIoTHubMQTTClient::changeEventTo(AzureIoTHubMQTTClient::AzureIoTHubMQTTClientEvent event) {
-
     currentEvent_ = event;
 
     if (eventCallback_) {
--- src/AzureIoTHubMQTTClient.h
+++ src/AzureIoTHubMQTTClient.h
@@ -1,9 +1,7 @@
 //
 // Created by Andri Yadi on 10/29/16.
 //
-
-#ifndef PIOMQTTAZUREIOTHUB_AZUREIOTHUBMQTT_H
-#define PIOMQTTAZUREIOTHUB_AZUREIOTHUBMQTT_H
+#pragma once
 
 #include <Arduino.h>
 #include "PubSubClient.h"
@@ -18,7 +16,6 @@
 #define DEBUG
 
 #ifdef DEBUG
-#define DEBUG_NTPCLIENT
 #define DEBUGLOG(...) DEBUG_PORT.printf(__VA_ARGS__)
 #else
 #define DEBUGLOG(...)
@@ -26,7 +23,6 @@
 
 #define MAX_JSON_OBJECT_SIZE 20
 #define AZURE_IOTHUB_MQTT_PORT    8883
-#define AZURE_IOTHUB_TOKEN_EXPIRE   10*24*3600 //seconds
 
 class AzureIoTHubMQTTClient : public PubSubClient {
 public:
@@ -39,20 +35,16 @@ public:
     };
 
     typedef std::function<void(const AzureIoTHubMQTTClientEvent event)> EventCallback;
-
     typedef std::function<void(String, JsonVariant)> AzureIoTHubMQTTClientCommandCallback;
     typedef std::map<String, AzureIoTHubMQTTClientCommandCallback> CommandsHandlerMap_t;
-
-    AzureIoTHubMQTTClient(Client& c, String iotHubHostName, String deviceId, String deviceKey);
-    ~AzureIoTHubMQTTClient();
-
     typedef std::map<const char*, JsonVariant> KeyValueMap;
 
-    bool begin();
+    AzureIoTHubMQTTClient(Client& c, String iotHubHostName, String deviceId, String deviceKey);
+    ~AzureIoTHubMQTTClient() { }
+
     void run();
     void end();
     bool sendEvent(String payload);
-    bool sendEvent(const uint8_t *payload, uint32_t plength, bool retained = false);
     void sendEventWithKeyVal(KeyValueMap keyValMap);
 
     void onEvent(EventCallback cb) {
@@ -67,8 +59,7 @@ private:
     String iotHubHostName_;
     String deviceId_;
     String deviceKey_;
-    String sasToken_;
-    String mqttCommandSubscribeTopic_, mqttCommandPublishTopic_;
+    String mqttCommandPublishTopic_;
     PubSubClient::callback_t onSubscribeCallback_;
 
     bool parseMessageAsJson_ = false;
@@ -77,12 +68,8 @@ private:
     CommandsHandlerMap_t commandsHandlerMap_;
 
     bool doConnect();
-    String createIotHubSASToken(char *key, String url, long expire = 0);
     void _onActualMqttMessageCallback(const MQTT::Publish& p);
 
     AzureIoTHubMQTTClientEvent currentEvent_ = AzureIoTHubMQTTClientEventUnknown;
     void changeEventTo(AzureIoTHubMQTTClientEvent event);
 };
-
-
-#endif //PIOMQTTAZUREIOTHUB_AZUREIOTHUBMQTT_H
